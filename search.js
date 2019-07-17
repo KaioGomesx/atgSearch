1ed901365b9cd23916030b0db74908bd6a415200
r1s3 <kaiogomes.nave@gmail.com>
2019-07-17 18:36:29 -0300

    Update search.js

--- search.js
+++ search.js
@@ -6,11 +6,9 @@ const searchOpts = {
   facets: 'port:10001'
 };
 
-// a função main é a responsavel por fazer a requisição ao shodan
 const main = () => {
     client
         .search('I20100', apiKey, searchOpts)
-        //pegando 
         .then(res => {
             result = res.matches.forEach(item => {
             fs.appendFile('hosts.txt', `${item['ip_str']}\n`, (err) => {
@@ -25,8 +23,7 @@ const main = () => {
     });
 }
 
-// Verificando se o arquivo "hosts.txt" existe, se existir 
-// será deletado e criado um novo contendo os novos hosts
+
 fs.open('hosts.txt', 'r', (err, fd) => {
     if (err) {
         if (err.code === 'ENOENT') {
@@ -36,7 +33,6 @@ fs.open('hosts.txt', 'r', (err, fd) => {
     } else {
         console.log("O arquivo 'hosts.txt' já existe, será deleta-lo para criar um novo para guardar os hosts que serão retornados agora")
         
-        // Deletando o arquivo hosts.txt e chamando a função main(que irá criar outro "hosts.txt")
         fs.unlink('hosts.txt', (err) => {
             if (err) throw err;
             console.log('hosts.txt deletado com sucesso, iremos agora criar outro com os hosts da próxima consulta');
